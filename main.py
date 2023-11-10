35378ededafb8f77696fdebda282bad3d000f383
dosxana@gmail.com <dosxana@gmail.com>
2023-11-10 20:38:52 +0600

    Initial commit

--- main.py
+++ main.py
@@ -9,27 +9,20 @@ class Item(BaseModel):
     description: str | None = None
     price: float
     tax: float | None = None
-
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "name": "Foo",
-                    "description": "A very nice Item",
-                    "price": 35.4,
-                    "tax": 3.2,
-                }
-            ]
-        }
-    }
+    tags: list[str] = []
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
 
 
-"""
-Declare Request Example Data¶
-"""
+@app.get("/items/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+'''
+Response Model - Return Type
+'''

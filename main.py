42ba49a78d077d7719fa328c89077bde8ba41b1a
dosxana@gmail.com <dosxana@gmail.com>
2023-11-13 20:39:28 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
 
 
-@app.post("/items/")
-async def create_item(item: Item) -> Item:
-    return item
-
-
-@app.get("/items/")
-async def read_items() -> list[Item]:
-    return [
-        Item(name="Portal Gun", price=42.0),
-        Item(name="Plumbus", price=32.0),
-    ]
-'''
-Response Model - Return Type
-'''
+# Don't do this in production!
+@app.post("/user/")
+async def create_user(user: UserIn) -> UserIn:
+    return user
